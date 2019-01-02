18fb701c1c573f9dfa20a27d6d3d5343d1f10f80
Jasper de Gooijer <j.degooijer@triple-it.nl>
2019-01-02 15:38:09 +0100

    added some comments and made a doublecheck that secures the fact that the bot only fires if a chat message is send to one of the target channels

--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,19 @@ var router = express.Router()
 
 // Initialize Discord Bot
 var bot = new Discord.Client();
-const d0xChannelId = "529986548079394819" // Test ID
-const finlabsChannelId = "509760275885588500" // Right ID
-
-bot.on('ready', function (evt) {
-  console.log(`Logged in as ${bot.user.tag}!`);
-});
+const d0xChannelId = "529986548079394819" // Right ID: [TODO] | Test ID: "529986548079394819"
+const finlabsChannelId = "529986518459088897" // Right ID: "509760275885588500"| Test ID: "529986518459088897"
 
 bot.on('message', msg => {
-  if (`${msg.author.username + "#" + msg.author.discriminator}` !== bot.user.tag) {
-    if (msg.channel.id === d0xChannelId) {
-      bot.channels.get(finlabsChannelId).send(msg.author.username + "#" + msg.author.discriminator + ": " + msg.content);
-    } else if (msg.channel.id === finlabsChannelId) {
-      bot.channels.get(d0xChannelId).send(msg.author.username + "#" + msg.author.discriminator + ": " + msg.content);
+  // Only continues when it's either the d0x or finlabs channel
+  if (msg.channel.id === "529986548079394819" || msg.channel.id === "529986518459088897") {
+    // Too prevent infinate loop between the bot and itself
+    if (`${msg.author.username + "#" + msg.author.discriminator}` !== bot.user.tag) {
+      if (msg.channel.id === d0xChannelId) {
+        bot.channels.get(finlabsChannelId).send(msg.author.username + "#" + msg.author.discriminator + ": " + msg.content);
+      } else if (msg.channel.id === finlabsChannelId) {
+        bot.channels.get(d0xChannelId).send(msg.author.username + "#" + msg.author.discriminator + ": " + msg.content);
+      }
     }
   }
 });
